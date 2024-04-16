3eac6beda1edac13ed7167b815c8f74a37e773ca
0x1Shub <shubhamyeram13@gmail.com>
2024-04-16 20:55:20 +0530

    Initial Commit

---
+++ client/src/App.tsx
@@ -0,0 +1,15 @@
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+
+const App = () => {
+  return (
+    <BrowserRouter>
+
+    {/* Navbar */}
+      <Routes>
+        <Route  />
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
